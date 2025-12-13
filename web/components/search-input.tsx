44f129bfe8a97f8f6949b2efc590f6e864f9c0a1
AdamHarish25 <adamharits25@gmail.com>
2025-12-13 17:08:10 +0700

    Re deploy

--- web/components/search-input.tsx
+++ web/components/search-input.tsx
@@ -47,6 +47,8 @@ export function SearchInput() {
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                 />
             </svg>
+
+            //New Deployment
         </div>
     )
 }
